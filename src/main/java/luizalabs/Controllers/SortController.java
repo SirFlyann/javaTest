dd63c6ec3814e0ae0add6b6d78e1ba4f135c5171
Mahezer Carvalho de Melo <mahezer.melo@luizalabs.com>
2019-06-09 15:03:04 -0300

    Add endpoint

---
+++ src/main/java/luizalabs/Controllers/SortController.java
@@ -0,0 +1,34 @@
+package luizalabs.Controllers;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import luizalabs.Models.Items;
+import luizalabs.Repositories.SortRepository;
+
+@RestController
+public class SortController {
+  
+  @RequestMapping(
+      value="/sort", 
+      method = RequestMethod.POST,
+      consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public ResponseEntity<?> sort(@RequestBody Items items) {
+    Map<String, String> filters = SortRepository.getFilters(items);
+    try {
+      Items filteredItems = SortRepository.applyFiltersToObject(filters, items);
+      return new ResponseEntity<Items>(filteredItems, HttpStatus.OK);
+    } catch(NoSuchFieldException e) {
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+  }
+
+}
